f08a17c3ca91eba769851f44044122b5ebe5c105
Jayden <jaydenfoo02@gmail.com>
2024-09-01 18:46:23 +0800

    Users

---
+++ routes/users_routes.js
@@ -0,0 +1,10 @@
+const express = require("express");
+
+// const { check } = require("express-validator");
+
+// const usersController = require("../controllers/users-controller");
+
+const router = express.Router();
+
+// Export the function
+module.exports = router;
