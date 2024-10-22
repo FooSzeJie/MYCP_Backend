f13445d4c4ecc7fbb0fb8b5bf561d82f55b95469
Jayden <jaydenfoo02@gmail.com>
2024-10-22 21:26:09 +0800

    Create Vehicle Function

--- controllers/Vehicle_Controller.js
+++ controllers/Vehicle_Controller.js
@@ -2,9 +2,82 @@ const { validationResult } = require("express-validator");
 
 // Import Model
 const Vehicle = require("../models/Vehicle");
+const User = require("../models/user");
 const HttpError = require("../models/Http_Error");
 
-const createVehicle = async (req, res, next) => {};
+const mongoose = require("mongoose");
+
+const createVehicle = async (req, res, next) => {
+  // Validator the Error
+  const errors = validationResult(req);
+
+  // if having error
+  if (!errors.isEmpty()) {
+    console.log(errors);
+    return next(
+      new HttpError("Invalid inputs passed, please check your data", 422)
+    );
+  }
+
+  const { license_plate, brand, color, creator } = req.body;
+
+  const createdVehicle = new Vehicle({
+    license_plate, // title : title;
+    brand,
+    color,
+    creator,
+    // creator: req.userData.userId,
+  });
+
+  let user;
+
+  try {
+    user = await User.findById(creator);
+    //   user = await User.findById(req.userData.userId);
+  } catch (e) {
+    const error = new HttpError(
+      "Creating Vehicle failed, please try later",
+      500
+    );
+
+    return next(error);
+  }
+
+  if (!user) {
+    const error = new HttpError("User not available", 404);
+
+    return next(error);
+  }
+
+  console.log(user);
+
+  try {
+    // sess === session
+    // Starting the Session
+    const sess = await mongoose.startSession();
+
+    // Starting The Transition
+    sess.startTransaction();
+
+    // Store the data into db
+    await createdVehicle.save({ session: sess });
+
+    // Push is one of the mongoose method to store the vehicle id to user table vehicle attributes
+    user.vehicles.push(createdVehicle);
+
+    // Store the data to the db by User Model
+    await user.save({ session: sess });
+
+    // Submit the Transition, only this step will update the db
+    await sess.commitTransaction();
+  } catch (e) {
+    const error = new HttpError("Created Fail !", 500);
+
+    return next(error);
+  }
+
+  res.status(201).json({ vehicle: createdVehicle });
+};
 
 // Export the Function
 exports.createVehicle = createVehicle;
--- routes/Vehicle_Route.js
+++ routes/Vehicle_Route.js
@@ -4,9 +4,22 @@ const { check } = require("express-validator");
 
 const vehicle_controller = require("../controllers/Vehicle_Controller");
 
+const checkAuth = require("../middlewares/Check_Auth");
+
 const router = express.Router();
 
-router.post("/create", vehicle_controller.createVehicle);
+// Under this function will need to login first
+// router.use(checkAuth);
+
+router.post(
+  "/create",
+  [
+    check("license_plate").not().isEmpty(),
+    check("brand").not().isEmpty(),
+    check("color").not().isEmpty(),
+  ],
+  vehicle_controller.createVehicle
+);
 
 // Export the Function
 module.exports = router;
