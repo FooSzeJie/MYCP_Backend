7063872b53a83b2d681e10c0c896bdd1dfc7d66d
Jayden <jaydenfoo02@gmail.com>
2024-10-27 20:50:36 +0800

    Local Authority Model

---
+++ models/Local_Authority.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const uniqueValidator = require("mongoose-unique-validator");
+
+const Schema = mongoose.Schema;
+
+const localAuthoritySchema = new Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  no_telephone: { type: Number, required: true, unique: true },
+  area: { type: String, required: true },
+  state: { type: String, required: true },
+});
+
+// Use the Validator function
+localAuthoritySchema.plugin(uniqueValidator);
+
+module.exports = mongoose.model("Local_Authority", localAuthoritySchema);
