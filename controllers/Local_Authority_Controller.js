66ee97fe359149fa5b0265c62c16d8189cefe20e
Jayden <jaydenfoo02@gmail.com>
2024-10-27 21:43:19 +0800

    Show Local Authority Function

--- controllers/Local_Authority_Controller.js
+++ controllers/Local_Authority_Controller.js
@@ -1,11 +1,63 @@
 const { validationResult } = require("express-validator");
 
 // Import Model
-const User = require("../models/user");
+// const User = require("../models/user");
 const Local_Authority = require("../models/Local_Authority");
 const HttpError = require("../models/Http_Error");
 
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
+
+const getLocalAuthority = async (req, res, next) => {
+  let localAuthorityList;
+
+  try {
+    localAuthorityList = await Local_Authority.find();
+  } catch (e) {
+    const error = new HttpError(
+      "Fetching local authorities failed, please try again.",
+      500
+    );
+  }
+
+  if (!localAuthorityList || localAuthorityList.length === 0) {
+    const error = new HttpError("No local authority found", 500);
+    return next(error);
+  }
+
+  res.json({
+    localAuthority: localAuthorityList.map((localAuthority) =>
+      localAuthority.toObject({ getters: true })
+    ),
+  });
+};
+
+const getLocalAuthorityById = async (req, res, next) => {
+  const localAuthorityId = req.params.laid;
+
+  let localAuthority;
+
+  try {
+    localAuthority = await Local_Authority.findById(localAuthorityId);
+  } catch (e) {
+    const error = new HttpError(
+      "Something went wrong, The user is not found",
+      500
+    );
+
+    return next(error);
+  }
+
+  if (!localAuthority) {
+    const error = new HttpError("Local authority not found", 404);
+
+    return next(error);
+  }
+
+  //   Show the Item without id have underscore
+  return res.json({
+    localAuthority: localAuthority.toObject({ getters: true }),
+  });
+};
 
 // Admin Login Function
 const createLocalAuthority = async (req, res, next) => {
@@ -45,8 +97,10 @@ const createLocalAuthority = async (req, res, next) => {
     .json({ localAuthority: "Local Authority created successfully" });
 };
 
-const editLocalAuthority = async (req, res, next) => {};
+const updateLocalAuthority = async (req, res, next) => {};
 
 // Export the Function
+exports.getLocalAuthority = getLocalAuthority;
+exports.getLocalAuthorityById = getLocalAuthorityById;
 exports.createLocalAuthority = createLocalAuthority;
-exports.editLocalAuthority = editLocalAuthority;
+exports.updateLocalAuthority = updateLocalAuthority;
--- routes/Local_Authority_Route.js
+++ routes/Local_Authority_Route.js
@@ -6,6 +6,10 @@ const local_Authority_controller = require("../controllers/Local_Authority_Contr
 
 const router = express.Router();
 
+router.get("/list", local_Authority_controller.getLocalAuthority);
+
+router.get("/:laid/profile", local_Authority_controller.getLocalAuthorityById);
+
 router.post(
   "/create",
   [
@@ -21,6 +25,23 @@ router.post(
     check("state").not().isEmpty(),
   ],
   local_Authority_controller.createLocalAuthority
+);
+
+router.patch(
+  "/:laid/update",
+  [
+    check("name").not().isEmpty(),
+    check("nickname").not().isEmpty(),
+    check("email").normalizeEmail().isEmail(), // Test@gmail.com => test@gmail.com
+    check("no_telephone")
+      .isNumeric()
+      .withMessage("Telephone number must be numeric")
+      .isLength({ min: 9, max: 12 })
+      .withMessage("Telephone number must be between 10 and 15 digits"),
+    check("area").not().isEmpty(),
+    check("state").not().isEmpty(),
+  ],
+  local_Authority_controller.updateLocalAuthority
 );
 
 module.exports = router;
