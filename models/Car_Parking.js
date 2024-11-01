dba840d7761510518bdf8d26f6394f9432db01a5
Jayden <jaydenfoo02@gmail.com>
2024-11-01 21:33:14 +0800

    Car Parking Model

---
+++ models/Car_Parking.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const uniqueValidator = require("mongoose-unique-validator");
+
+const Schema = mongoose.Schema;
+
+const carParkingSchema = new Schema({
+  starting_time: { type: Date, required: true }, // Use Date type for timestamps
+  end_time: { type: Date, required: true }, // Use Date type for timestamps
+  duration: { type: Number, required: true },
+  local_authority: { type: String, required: true },
+  vehicles: { type: String, required: true },
+  status: {
+    type: String,
+    required: true,
+    enum: ["complete", "ongoing"],
+    default: "ongoing", // Set a valid default value within the enum
+  },
+});
+
+// Use the uniqueValidator plugin to enforce uniqueness constraints
+carParkingSchema.plugin(uniqueValidator);
+
+module.exports = mongoose.model("Car_Parking", carParkingSchema);
