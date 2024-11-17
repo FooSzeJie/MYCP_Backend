0ec28f25484658bef084955946036f842618fee4
Jayden <jaydenfoo02@gmail.com>
2024-11-18 00:38:12 +0800

    Updated Saman Model

--- models/Saman.js
+++ models/Saman.js
@@ -9,6 +9,21 @@ const samanSchema = new Schema({
 
   date: { type: Date, required: true },
 
+  status: {
+    type: String,
+    required: true,
+    enum: ["unpaid", "paid"],
+    default: "unpaid",
+  },
+
+  price: {
+    type: Number,
+    required: true,
+    get: (v) => v / 100,
+    set: (v) => Math.round(v * 100),
+    default: 50,
+  },
+
   vehicle: { type: mongoose.Types.ObjectId, require: true, ref: "Vehicle" },
 
   creator: { type: mongoose.Types.ObjectId, require: true, ref: "User" },
